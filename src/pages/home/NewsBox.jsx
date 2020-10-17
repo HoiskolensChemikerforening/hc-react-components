e6f9096accdd2c8eef22ece3c4cfce3f71379e32
Anja Rosvold From <anjarfrom@gmail.com>
2020-10-17 13:01:07 +0200

    Add fetch Token method.

--- src/pages/home/NewsBox.jsx
+++ src/pages/home/NewsBox.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "../../index.css";
-import { NewsListing } from "./NewsListing";
-import { Col, Row } from "../../components/FlexLayout";
-import { P } from "../../components/Text";
+import {NewsListing} from "./NewsListing";
+import {Col, Row} from "../../components/FlexLayout";
+import {P} from "../../components/Text";
+
 
 function NewsBox() {
   // Declares a var articles, and an update function, useState sets initial value to an empty list.
@@ -30,44 +31,29 @@ function NewsBox() {
     });
     const jsonData = await response.json();
     console.log(jsonData);
+
   }
 
   // Gets all articles from the news api url and sets the articles variable
   async function fetchArticlesData() {
-    const response = await fetch("http://localhost:8000/nyheter/api/", {
-      headers: {
-        Authorization: "Token cd17a0b64b963c31156e27beca0bb61db1b5b318",
-      },
-    });
+    const response = await fetch("http://localhost:8000/nyheter/api/", );
     const jsonData = await response.json();
     setLatestArticle(jsonData[0]);
-    setArticles(jsonData.slice(1));
   }
 
   return (
-    <Col flex={10}>
-      {latestArticle ? (
-        <NewsListing
-          key={latestArticle.id}
-          article={latestArticle}
-          isMainArticle={true}
-        />
-      ) : (
-        <P>Ingen artikler finnes</P>
-      )}
+      <Col flex={10}>
+        {latestArticle ? <NewsListing key={latestArticle.id} article={latestArticle} isMainArticle={true}/>
+            : <P>Ingen artikler finnes</P>}
 
-      <br />
-      <Row flex={10}>
-        {articles.map(article => (
-          <NewsListing
-            key={article.id}
-            article={article}
-            isMainArticle={false}
-          />
-        ))}
-      </Row>
-    </Col>
+        <br/>
+        <Row flex={10}>
+          {articles.map(article => (
+              <NewsListing key={article.id} article={article} isMainArticle={false}/>
+          ))}
+        </Row>
+      </Col>
   );
 }
 
-export { NewsBox };
+export {NewsBox};
